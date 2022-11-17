e1927a599ec306aac0a3e0c7537543083d695020
Ltfjx <ltfjx2333@gmail.com>
2022-11-17 13:13:31 +0800

    修改文件结构

--- app.ts
+++ app.ts
@@ -4,13 +4,14 @@ import express from 'express';
 const app = express();
 
 const port = 80
+const sitename = "Potato Utils" //显示在网页标题上（ToolName | XXX）
 
 app.listen(port,()=>{
   console.log(`PoatoUtils Server started on port ${port} `)
 })
 
 app.get('/utils/:name', (req, res) => {
-    ejs.renderFile(`utils/${req.params.name}/index.ejs`).then(value => {
+    ejs.renderFile(`utils/${req.params.name}/index.ejs`,{"sitename":sitename}).then(value => {
         res.send(value);
     })
 });
