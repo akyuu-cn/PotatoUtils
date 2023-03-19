cca6141f52d585a78da15cde89069329fabf2797
Ltfjx <ltfjx2333@gmail.com>
2023-03-19 14:34:26 +0800

    rua!!!!

--- app.ts
+++ app.ts
@@ -1,17 +1,35 @@
-import ejs from 'ejs';
-import express from 'express';
+import ejs from 'ejs'
+import express from 'express'
+import fetch from 'node-fetch'
 
 const app = express();
+
+
+
 
 const port = 80
 const sitename = "Potato Utils" //显示在网页标题上（ToolName | SiteName）
 
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`PoatoUtils Server started on port ${port} `)
 })
 
 app.get('/utils/:name', (req, res) => {
-    ejs.renderFile(`utils/${req.params.name}/index.ejs`,{"sitename":sitename}).then(value => {
-        res.send(value);
-    })
-});
+  ejs.renderFile(`utils/${req.params.name}/index.ejs`, { "sitename": sitename }).then(value => {
+    res.send(value)
+  })
+})
+
+app.get('/js/:name', (req, res) => {
+  ejs.renderFile(`js/${req.params.name}`).then(value => {
+    res.send(value)
+  })
+})
+
+app.get('/data/:name/:dname', (req, res) => {
+  ejs.renderFile(`data/${req.params.name}/${req.params.dname}`).then(value => {
+    res.send(value)
+  })
+})
+
+
