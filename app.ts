e4e3fedbf5e5e9b7b20312ced00df559acfb73fa
Ltfjx <ltfjx2333@gmail.com>
2023-05-26 21:13:11 +0800

    footer & code clean up

--- app.ts
+++ app.ts
@@ -1,6 +1,5 @@
-import ejs, { fileLoader } from 'ejs'
-import express, { json } from 'express'
-import fetch from 'node-fetch'
+import ejs from 'ejs'
+import express from 'express'
 import fs from 'fs'
 
 const app = express()
@@ -8,6 +7,7 @@ const app = express()
 
 const port = 80 // 运行端口
 const sitename = "Potato Utils" // 显示在网页标题上（ToolName | SiteName）
+let config = fs.readFileSync('config.yml')
 
 app.listen(port, () => {
   console.log(`PoatoUtils Server started on port ${port} `)
@@ -15,9 +15,8 @@ app.listen(port, () => {
 
 // utils 页面
 app.get('/utils/:name', (req, res) => {
-  ejs.renderFile(`utils/${req.params.name}/index.ejs`, { "sitename": sitename }).then(value => {
-    res.send(value)
-  })
+  ejs.renderFile(`utils/${req.params.name}/index.ejs`, { "sitename": sitename })
+  .then(value => {res.send(value)})
 })
 
 // js 请求处理
@@ -32,19 +31,16 @@ app.get('/css/:name', (req, res) => {
 
 
 // data 请求处理
-app.get('/data/:name/:dname', (req, res) => {
-  ejs.renderFile(`data/${req.params.name}/${req.params.dname}`).then(value => {
-    res.send(value)
-  })
+app.get('/data/:utilsName/:name', (req, res) => {
+  res.send(fs.readFileSync(`data/${req.params.utilsName}/${req.params.name}`))
 })
 
 // 主页
 app.get('/', (req, res) => {
   let data = fs.readFileSync('data/index/utils_list.json', "utf-8")
   let jsonData = JSON.parse(data)
-  ejs.renderFile(`pages/index.ejs`, { "data": jsonData }).then(value => {
-    res.send(value)
-  })
 
+  ejs.renderFile(`pages/index.ejs`, { "data": jsonData })
+  .then(value => {res.send(value)})
 })
 
